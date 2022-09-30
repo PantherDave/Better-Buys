b82e5abe5059dedb8b9a0ef4c0584367e03c3973
David Acosta <dacostacsit@gmail.com>
2022-09-30 19:09:37 -0400

    Database functions

--- includes/Database.php
+++ includes/Database.php
@@ -18,4 +18,45 @@ class Database {
             die('Connection Error : '.mysqli_connect_error());
         }
     }
+
+    // Executing SQL Query
+    public function query($sql) {
+        $result = $this->connection->query($sql);
+
+        if (!$result) {
+            die('Query fails : '.$sql);
+        }
+
+        return $result;
+    }
+
+    // Fetching list of data from the SQL query result
+    public function fetch_array($result) {
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $result_array[] = $row;
+            }
+
+            return $result_array;
+        }
+    }
+
+    // Fetching a single row of data from the SQL query
+    public function fetch_row($result)
+    {
+        if ($result->num_rows > 0)
+            return $result->fetch_assoc();
+    }
+
+    // Check proper format of the data
+    public function escape_value($value) {
+        return $this->connection->real_escape_string($value);
+    }
+
+    // Closes the connection with SQL
+    public function close_connection() {
+        $this->connection->close();
+    }
 }
+
+$database = new Database();
